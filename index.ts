c53eb517e75191b94a8bcdb523ea27de10009765
Dana bestun <ensw00823@student.su.edu.krd>
2025-03-30 22:27:23 +0300

    t

--- index.ts
+++ index.ts
@@ -12,7 +12,8 @@ const app = new Elysia();
 app.use(cors());
 app.use(todoRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
-}
+app. listen(process.env.PORT ?? 3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
